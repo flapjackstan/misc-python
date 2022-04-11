5ca3ea7b57ed1782454270f134b313cc4aee4260
flapjackstan <elm.s.camargo@gmail.com>
2022-04-11 09:05:25 -0700

    added script that returns first available drive

---
+++ check_drives.py
@@ -0,0 +1,23 @@
+import os
+
+
+def get_open_drive():
+    for i in range(ord('A'), ord('Z') + 1):
+
+        drive = chr(i) + ':'
+        print('Trying drive:' + drive)
+
+        try:
+            os.scandir(drive)
+        except Exception:
+            print("Drive is Empty")
+            return drive
+
+
+def main():
+    drive = get_open_drive()
+    print(drive)
+
+
+if __name__ == '__main__':
+    main()
